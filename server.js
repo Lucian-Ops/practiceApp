f1e22e26d6f8338d26833ff345d3a575a938257b
Lucian <lucian.cimpeanu96@gmail.com>
2021-01-03 14:48:44 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// This brings in the express server side framework
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello practice app v1!");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port} - ${Date.now()}`);
+});
